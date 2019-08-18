98c3ae9a13b711cd6345c07197ceddb6ea13048d
zhangzw16 <THUzhangzw@outlook.com>
2019-08-18 23:21:32 +0800

    delete config

--- server/config.js
+++
@@ -1,4 +0,0 @@
-export const port = process.env.PORT || 3000;
-export const connexionString = 'mongodb+srv://zhangzw:Zz123456@cluster0-rc3fq.mongodb.net/test?retryWrites=true/';
-export const baseApi = 'api';
-export const saltTimes = 10;
